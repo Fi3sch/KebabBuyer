3fd43cd608b55e2204fc1acc97d1470b82a5646c
fi3sch <sadrir@hotmail.com>
2015-11-10 09:00:51 +0100

    KebabBuyer

---
+++ tasks/Task.java
@@ -0,0 +1,18 @@
+package tasks;
+
+import main.*;
+
+import org.powerbot.script.ClientAccessor;
+import org.powerbot.script.ClientContext;
+
+
+@SuppressWarnings("rawtypes")
+public abstract class Task<C extends ClientContext> extends ClientAccessor<C> {
+    public Task(C ctx) {
+        super(ctx);
+    }
+
+    public abstract boolean activate();
+
+    public abstract void execute();
+}
